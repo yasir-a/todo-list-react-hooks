ed24d238a4b30d98f617850eddd5f6159fd5fc0d
yasir-a <yasir.techhuset@gmail.com>
2022-05-29 01:08:05 +0500

    non optimized

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
